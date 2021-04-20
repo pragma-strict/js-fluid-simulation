af6aaed3f22321bc13afdb51460c703851eb991a
Ian <ianrdejong@gmail.com>
2021-04-19 21:40:43 -0700

    Add function to get immediate cell kernel

--- js/main.js
+++ js/main.js
@@ -32,12 +32,53 @@ function windowResized() {
 }
 
 
+// A single simulation step
+function tick(){
+
+}
+
+
+// Return list of immediate neighbors to given cell
+function getCellKernel(index){
+  let kernel = [
+    index,
+    getCellUp(index),
+    getCellDown(index),
+    getCellLeft(index),
+    getCellRight(index),
+    getCellLeft(getCellUp(index)),
+    getCellRight(getCellUp(index)),
+    getCellLeft(getCellDown(index)),
+    getCellRight(getCellDown(index))
+  ];
+  return kernel;
+}
+
+function getCellUp(index){
+  return (index - world_size) >= 0 ? (index - world_size) : -1;
+}
+
+function getCellDown(index){
+  return (index + world_size) < (world_size * world_size) ? (index + world_size) : index % world_size;
+}
+
+function getCellRight(index){
+  return (index % world_size) != (world_size -1) ? (index + 1) : (index - (world_size -1) );
+}
+
+function getCellLeft(index){
+  return (index % world_size) != 0 ? (index - 1) : (index + (world_size -1) );
+}
+
+
 function draw(){
   drawFrame();
   let cellUnderMouse = screenToCellSpace(createVector(mouseX, mouseY));
   let mouseVelocity = createVector(mouseX - pmouseX, mouseY - pmouseY);
   if(cellUnderMouse != -1){
     cells[cellUnderMouse] = mouseVelocity;
+    colorCell(cellUnderMouse, color(0, 0, 0));
+    colorCells(getCellKernel(cellUnderMouse), 0);
   }
 }
 
@@ -63,6 +104,13 @@ function colorCell(index, color){
   fill(color);
   let cellCoordinate = cellToScreenSpace(index);
   rect(cellCoordinate.x, cellCoordinate.y, cell_size, cell_size);
+}
+
+
+function colorCells(indexes, color){
+  for(let i = 0; i < indexes.length; i++){
+    colorCell(indexes[i], color);
+  }
 }
 
 
