9426de942f01e53f25139d78d5f61ef45231d00a
Ian <ianrdejong@gmail.com>
2021-04-19 22:18:55 -0700

    Add visual polish

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 
 let canvas;
-let mouseIsPressed = false;
+
+let WORLD_SIZE = 64;
+let CELL_SIZE = 8;
+let HALF_CELL_SIZE = 8/2;
+
 let cells = [];
-let world_size = 32;
-let cell_size = 15;
+let cellPositionOffset;
 
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   repositionCanvas();
-  for(let i = 0; i < world_size * world_size; i++){
+  cellPositionOffset = createVector(round(width/2 - (WORLD_SIZE * CELL_SIZE) /2), round(height/2 - (WORLD_SIZE * CELL_SIZE) /2));
+  for(let i = 0; i < WORLD_SIZE * WORLD_SIZE; i++){
     cells.push(createVector(0, 0));
   }
   noStroke();
-  drawFrame();
+  render();
 }
 
 
@@ -22,18 +26,28 @@ function repositionCanvas()
 	var x = windowWidth - width;
 	var y = windowHeight - height;
 	canvas.position(x, y);
+  cellPositionOffset = createVector(round(width/2 - (WORLD_SIZE * CELL_SIZE) /2), round(height/2 - (WORLD_SIZE * CELL_SIZE) /2));
 }
 
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 	repositionCanvas();
-	drawFrame();
+  cellPositionOffset = createVector(round(width/2 - (WORLD_SIZE * CELL_SIZE) /2), round(height/2 - (WORLD_SIZE * CELL_SIZE) /2));
+	render();
 }
 
 
 // A single simulation step
 function tick(){
+  // Apply mouse velocity to cells
+  let cellUnderMouse = screenToCellSpace(createVector(mouseX, mouseY));
+  let mouseVelocity = createVector(mouseX - pmouseX, mouseY - pmouseY);
+  if(cellUnderMouse != -1){
+    cells[cellUnderMouse] = mouseVelocity.mult(10);
+  }
+
+  // Apply diffusion
   for(let i = 0; i < cells.length; i++){
     let kernel = getCellKernel(i);
     let velocitySum = createVector(0, 0);
@@ -42,6 +56,40 @@ function tick(){
     }
     cells[i] = velocitySum.div(kernel.length);
   }
+}
+
+
+function draw(){
+  tick();
+  render();
+}
+
+
+function render()
+{
+  background(BG_COL);
+
+  // Draw outline around simulation bounds
+  stroke(0);
+  strokeWeight(1);
+  fill(color(0, 0, 0, 0));
+  rect(cellPositionOffset.x - HALF_CELL_SIZE, cellPositionOffset.y - HALF_CELL_SIZE, CELL_SIZE * WORLD_SIZE, CELL_SIZE * WORLD_SIZE);
+
+  // Render cell vectors
+  for(let i = 0; i < cells.length; i++){
+    let cellScreenPos = cellToScreenSpace(i);
+    drawVector(cells[i], cellScreenPos);
+  }
+
+  // Render text
+  fill(0);
+  noStroke();
+  textSize(24);
+  text("Vector Field Test", cellPositionOffset.x, cellPositionOffset.y - 15);
+  textSize(16);
+  text("FPS: " + floor(frameRate()), 
+    cellPositionOffset.x + (CELL_SIZE * WORLD_SIZE) - textWidth("FPS: 600"), 
+    cellPositionOffset.y + (CELL_SIZE * WORLD_SIZE) - 10);
 }
 
 
@@ -62,52 +110,25 @@ function getCellKernel(index){
 }
 
 function getCellUp(index){
-  return (index - world_size) >= 0 ? (index - world_size) : -1;
+  return (index - WORLD_SIZE) >= 0 ? (index - WORLD_SIZE) : -1;
 }
 
 function getCellDown(index){
-  return (index + world_size) < (world_size * world_size) ? (index + world_size) : index % world_size;
+  return (index + WORLD_SIZE) < (WORLD_SIZE * WORLD_SIZE) ? (index + WORLD_SIZE) : index % WORLD_SIZE;
 }
 
 function getCellRight(index){
-  return (index % world_size) != (world_size -1) ? (index + 1) : (index - (world_size -1) );
+  return (index % WORLD_SIZE) != (WORLD_SIZE -1) ? (index + 1) : (index - (WORLD_SIZE -1) );
 }
 
 function getCellLeft(index){
-  return (index % world_size) != 0 ? (index - 1) : (index + (world_size -1) );
-}
-
-
-function draw(){
-  tick();
-  drawFrame();
-  let cellUnderMouse = screenToCellSpace(createVector(mouseX, mouseY));
-  let mouseVelocity = createVector(mouseX - pmouseX, mouseY - pmouseY);
-  if(cellUnderMouse != -1){
-    cells[cellUnderMouse] = mouseVelocity.mult(10);
-  }
-}
-
-
-function drawFrame()
-{
-  background(BG_COL);
-
-  // Draw outline around simulation bounds
-  stroke(0);
-  strokeWeight(1);
-  fill(color(0, 0, 0, 0));
-  rect(0, 0, cell_size * world_size, cell_size * world_size);
-  for(let i = 0; i < cells.length; i++){
-    let cellScreenPos = cellToScreenSpace(i);
-    drawVector(cells[i], cellScreenPos);
-  }
+  return (index % WORLD_SIZE) != 0 ? (index - 1) : (index + (WORLD_SIZE -1) );
 }
 
 
 function drawVector(vector, position){
   strokeWeight(1);
-  stroke(256 - vector.mag()*256 / cell_size);
+  stroke(256 - vector.mag()*256 / CELL_SIZE);
   line(position.x, position.y, position.x + vector.x, position.y + vector.y);
 }
 
@@ -115,7 +136,7 @@ function drawVector(vector, position){
 function colorCell(index, color){
   fill(color);
   let cellCoordinate = cellToScreenSpace(index);
-  rect(cellCoordinate.x, cellCoordinate.y, cell_size, cell_size);
+  rect(cellCoordinate.x, cellCoordinate.y, CELL_SIZE, CELL_SIZE);
 }
 
 
@@ -127,21 +148,21 @@ function colorCells(indexes, color){
 
 
 function cellToScreenSpace(index){
-  x = index % world_size;
-  y = floor(index / world_size);
-  x *= cell_size;
-  y *= cell_size;
-  return createVector(x, y);
+  x = index % WORLD_SIZE;
+  y = floor(index / WORLD_SIZE);
+  x *= CELL_SIZE;
+  y *= CELL_SIZE;
+  return createVector(x + cellPositionOffset.x, y + cellPositionOffset.y);
 }
 
 
 function screenToCellSpace(point){
-  let x = floor(point.x / cell_size);
-  let y = floor(point.y / cell_size);
-  if(x >= world_size || y >= world_size){
+  let x = floor((point.x - cellPositionOffset.x) / CELL_SIZE);
+  let y = floor((point.y - cellPositionOffset.y) / CELL_SIZE);
+  if(x >= WORLD_SIZE || x < 0 || y >= WORLD_SIZE || y < 0){
     return -1;
   }
-  index = y * world_size;
+  index = y * WORLD_SIZE;
   index += x;
   return index;
 }
